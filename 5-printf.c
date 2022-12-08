99ac5647739fafa9da7a2066e436f95268b06758
janechege24 <janechege2476@gmail.com>
2022-12-08 15:51:18 -0800

    5-printf.c

--- 5-printf.c
+++ 5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  * main - main block
@@ -7,13 +6,10 @@
  *   * Return: 0
  *
  *    */
-
 int main(void)
 
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
-
 }
