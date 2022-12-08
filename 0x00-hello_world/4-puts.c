4fd11679cb3042718554d40116d51a398a98930d
janechege24 <janechege2476@gmail.com>
2022-12-08 15:38:22 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
 /**
- * 
- * main - print a line of code using puts
- * Return: 0
-*/
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
