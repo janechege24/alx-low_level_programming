250d21a5ec9c906341162340e047a6ea744cf163
janechege24 <janechege2476@gmail.com>
2022-12-12 07:11:30 -0800

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-#include <ctype.h>
 
 /**
- * main - program that prints the alphabet in lowercase, followed by a new line
- * You can only use the putchar
- * Return: 0
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
 
-	int l = 'a';
-
-	while (l <= 'z')
-
-	{
-
-		putchar(l);
-
-		l += 1;
-
-	}
-
-	putchar('\n');
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+	printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+	printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+	printf("and is less than 6 and not 0\n");
 
 	return (0);
-
 }
+
