cf244857119d0a9a604e9af04fde2f30cf3cfc51
janechege24 <janechege2476@gmail.com>
2022-12-13 09:53:38 -0800

    ./101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,26 +13,23 @@ int main(void)
 
 			for (m = 49; m < 58; m++)
 			{
-
+	
 				for (l = 50; l < 58; l++)
 				{
-					if (l > m && m > n)
+															if (l > m && m > n)
 					{
-						
 						putchar(n);
 						putchar(m);
 						putchar(l);
 						if (n != 55 || m != 56)
 						{
-							putchar(',');
+																											putchar(',');
 							putchar(' ');
-																									}
+						}
 					}
 				}
 			}
 		}
 		putchar('\n');
 		return (0);
-
 }
-%s/\s\+$//e
