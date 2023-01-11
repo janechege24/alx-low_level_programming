f472249c1dd4402d1150bb1d562cfc9b0de2c705
janechege24 <janechege2476@gmail.com>
2023-01-10 16:39:28 -0800

    ./a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,8 @@
-#include "main.h"
 #include <stdlib.h>
-
 /**
  * create_array - Main Entry
  * @size: input
  * @c: input
- *
  * Return: 0
  */
 char *create_array(unsigned int size, char c)
--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -11,23 +11,24 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+   unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
+
+   i = 0;
+   while (i < size)
+   {
         if (i % 10)
         {
-            printf(" ");
+           printf(" ");
         }
-        if (!(i % 10) && i)
+	if (!(i % 10) && i)
         {
-            printf("\n");
+	   printf("\n");
         }
         printf("0x%02x", buffer[i]);
         i++;
-    }
-    printf("\n");
+   }
+   printf("\n");
 }
 
 /**
